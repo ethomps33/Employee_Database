8503291a986f62c4102d361fe970745597479c46
ethomps33 <e.thomps33@gmail.com>
2021-02-25 15:07:09 -0700

    Added QuickDBD png

--- EmployeeSQL/Schema .sql
+++ EmployeeSQL/Schema .sql
@@ -1,17 +1,25 @@
-
 CREATE TABLE "department" (
     "dept_no" varchar(4)   NOT NULL,
-    "dept_name" varchar(20)   NOT NULL
+    "dept_name" varchar(20)   NOT NULL,
+    CONSTRAINT "pk_department" PRIMARY KEY (
+        "dept_no"
+     )
 );
 
 CREATE TABLE "dept_emp" (
     "emp_no" int   NOT NULL,
-    "dept_no" varchar(4)   NOT NULL
+    "dept_no" varchar(4)fk   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
 CREATE TABLE "dept_manager" (
     "dept_no" varchar(4)   NOT NULL,
-    "emp_no" int   NOT NULL
+    "emp_no" int   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
 CREATE TABLE "employees" (
@@ -21,16 +29,24 @@ CREATE TABLE "employees" (
     "first_name" varchar(20)   NOT NULL,
     "last_name" varchar(25)   NOT NULL,
     "sex" varchar(1)   NOT NULL,
-    "hire_date" varchar(15)   NOT NULL
+    "hire_date" varchar(15)   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
 CREATE TABLE "salaries" (
     "emp_no" int   NOT NULL,
-    "salary" int   NOT NULL
+    "salary" int   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
 CREATE TABLE "titles" (
     "title_id" varchar(5)   NOT NULL,
-    "title" varchar(20)   NOT NULL
+    "title" varchar(20)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
 );
-
